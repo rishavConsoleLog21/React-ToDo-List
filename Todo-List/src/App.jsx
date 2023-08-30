f06d1253fc29966a2f36129cb329bfdb9839ee7d
rishavcool21 <kumarrishavcool21@gmail.com>
2023-08-30 16:41:58 +0530

    Added Todo Component

---
+++ Todo-List/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import CssBaseline from "@mui/material/CssBaseline"
+import TodoList from './TodoList'
+function App() {
+  return (
+    <>
+     <CssBaseline />
+     <h1>Todo List</h1>
+     <TodoList />
+    </>
+  )
+}
+
+export default App
