691ba738db215e282ddabd02014e8626931568c2
rishavcool21 <kumarrishavcool21@gmail.com>
2023-09-01 01:12:30 +0530

    Added QuoteFetcher

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import CssBaseline from "@mui/material/CssBaseline"
 import TodoList from './TodoList'
 import Navbar from './Navbar'
+import QuoteFetcher from './QuoteFetcher'
 function App() {
   return (
     <>
      <CssBaseline />
      <Navbar />
      <TodoList />
+     <QuoteFetcher/>
     </>
   )
 }
